da7b3d610a7d0f1be3f3d81608c0ef83562f0680
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-02-10 16:59:31 -0600

    added sinatra to library form

--- main.rb
+++ main.rb
@@ -1,42 +1,14 @@
 require "sinatra"
-# require_relative"dinner_club"
-
-before do
-  @test = "This is a test of before filters."
-end
-
-helpers do
-  def foosplosion
-    @foo = "FOOBAR"*60
-  end
-end
 
 get "/" do
-  @user_name = "Steven"
-  foosplosion
   erb :welcome, :layout => :boilerplate
 end
 
-get "/greet/:name:age" do
-  
-  
-  "Hello, #{params[:name]}"
-  "You're #{params[:age]} years old"
+get "/library_form" do
+  erb :library_form, :layout => :boilerplate 
 end
 
-get "/show_foo" do
-  "FOO\n"*1000
+get "/results" do
+  @results = params
+  erb :results, :layout => :boilerplate
 end
-  
-  
-  
-  
-  
-# class User
-#   def initialize(options)
-#     @user_name = options[:name]
-#   end
-#
-# end#classend
-#
-# user = User.new({name: "Steven"})
